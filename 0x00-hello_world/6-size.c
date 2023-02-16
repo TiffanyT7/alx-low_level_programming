2af9727af5c49a2362f095b3c21f2cb2ab0fc506
Tiffany <tiffanythuynsma7@gmail.com>
2023-02-16 15:21:07 -0800

    using sizeof

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
-int main() 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void) 
 {
 	printf("Size of a char = %zu bytes \n", sizeof(char));
        	printf("Size of an int  = %zu bytes \n", sizeof(int));
