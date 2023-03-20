28ff1820f86cc2d536a5aa98b51fe3c180c140ce
Tiffany <tiffanythuynsma7@gmail.com>
2023-03-20 13:34:31 -0700

    New type, struct dog defined

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+#include <stdio.h>
+/**
+ * struct dog - new type
+ * @name: dogs name
+ * @age: dogs age
+ * @owner: dogs owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
