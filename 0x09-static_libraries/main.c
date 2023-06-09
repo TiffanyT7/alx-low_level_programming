13c40bf72d9e25027fd8a3e3910aaf7538c51950
root <tiffanythuynsma7@gmail.com>
2023-06-09 12:54:30 -0700

    betty style fixed

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,8 +1,13 @@
 #include "main.h"
 
+/**
+ * main - check code
+ * Return: 0
+ */
+
 int main(void)
 {
-    _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t"
+	_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t"
 "- Kevin Mitnick");
-    return (0);
+	return (0);
 }
